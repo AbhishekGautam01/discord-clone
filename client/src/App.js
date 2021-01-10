cba28e64d899f37efc38c577c30f54ddb2ab0dfc
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-10 20:15:20 +0530

    Added loggedin user check

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
 import './App.css';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     //BEM naming convention
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h2>You need to login </h2>
+      )}
     </div>
   );
 }
