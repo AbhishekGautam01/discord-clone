b193d3a0a379769956a7222dce780587c2d38867
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-10 19:34:33 +0530

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 import './App.css';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-      {/* Sidebar  */}
       <Sidebar />
-      {/* Chat */}
+      <Chat />
     </div>
   );
 }
