22d67bfb8d3c2a2a6333e6e685ffb6164c31c9e3
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-10 19:46:40 +0530

    Message Impl

---
+++ client/src/components/message/Message.js
@@ -0,0 +1,18 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './Message.css';
+function Message() {
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="message__info">
+        <h4>
+          Abhshek<span className="message__timestamp">timestamp</span>
+        </h4>
+        <p>This is a message</p>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
