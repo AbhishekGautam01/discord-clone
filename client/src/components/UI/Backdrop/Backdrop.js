a8a11f807e74dd75a63c3f33b865f216a6327aca
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-11 23:32:29 +0530

    Impl: Backdrop

---
+++ client/src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Backdrop.css';
+const Backdrop = ({ show, clicked }) =>
+  show ? <div className="backdrop" onClick={clicked}></div> : null;
+
+export default Backdrop;
